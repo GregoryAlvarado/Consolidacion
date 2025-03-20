77f93f92408c8e4ca800ecf9b808665185381489
GregoryAlvarado <133550653+GregoryAlvarado@users.noreply.github.com>
2025-03-20 04:17:31 -0500

    ConsolidacionExplicito.py

--- ConsolidacionExplicito.py
+++ ConsolidacionExplicito.py
@@ -41,93 +41,98 @@ with col1:
     U0 = st.number_input("Carga (kPa/m²)", min_value=0.1, value=100.0, step=1.0)
 
     st.markdown("""**Nota:** El valor de Cv debe ser menor a 0.5 para evitar inestabilidades numéricas.""")
-    
-# ECUACIONES 
-with col2:
-    st.header("Ecuaciones")
-    st.latex(r"T_v = \frac{C_v \cdot t}{H_{dr}^2}")
-    st.latex(r"Z = \frac{z}{H_{dr}}")
-    st.latex(r"U_z = 1 - \sum_{m=0}^{10} \frac{2}{M} \sin(M Z) e^{-M^2 T_v}")
-    st.latex(r"U_e = U_0 (1 - U_z)")
-    
-    # Método explícito
-    st.subheader("Método Explícito")
-    st.latex(r"U_i^{j+1} = k \cdot U_{i-1}^{j} + (1 - 2k) \cdot U_i^{j} + k \cdot U_{i+1}^{j}")
 
-# Cálculos
-Hdr = H / 2  # Se asume que el estrato inferior es permeable, por lo que Hdr = H / 2
-z = np.arange(0, H + 1, 1)  # Profundidad
-z_Hdr = z / Hdr
+    calcular = st.button("Calcular")
 
-# Diferencial de tiempo
-tiempos = np.arange(1, t_max + 1, 1) 
+if calcular:
+    # ECUACIONES 
+    with col2:
+        st.header("Ecuaciones")
+        st.latex(r"T_v = \frac{C_v \cdot t}{H_{dr}^2}")
+        st.latex(r"Z = \frac{z}{H_{dr}}")
+        st.latex(r"U_z = 1 - \sum_{m=0}^{10} \frac{2}{M} \sin(M Z) e^{-M^2 T_v}")
+        st.latex(r"U_e = U_0 (1 - U_z)")
+        
+        # Método explícito
+        st.subheader("Método Explícito")
+        st.latex(r"U_i^{j+1} = k \cdot U_{i-1}^{j} + (1 - 2k) \cdot U_i^{j} + k \cdot U_{i+1}^{j}")
 
-# CÁLCULOS PARA EL MÉTODO TEÓRICO
-resultados_teoricos = {f"{unidad_tiempo} {t} ": [] for t in tiempos}
+    # Cálculos
+    Hdr = H / 2  # Se asume que el estrato inferior es permeable, por lo que Hdr = H / 2
+    z = np.arange(0, H + 1, 1)  # Profundidad
+    z_Hdr = z / Hdr
 
-for t in tiempos:
-    Tv = calcular_Tv(Cv, t, Hdr)
-    Uz = calcular_Uz(Tv, z_Hdr)
-    Ue = calcular_Ue(Uz, U0)
-    
-    # Resultados de Ue para cada profundidad
-    for i, profundidad in enumerate(z):
-        resultados_teoricos[f"{unidad_tiempo} {t} "].append(Ue[i])
+    # Diferencial de tiempo
+    tiempos = np.arange(1, t_max + 1, 1) 
 
-df_resultados_teoricos = pd.DataFrame(resultados_teoricos, index=z)
-df_resultados_teoricos = df_resultados_teoricos.round(2)
+    # CÁLCULOS PARA EL MÉTODO TEÓRICO
+    resultados_teoricos = {f"{unidad_tiempo} {t} ": [] for t in tiempos}
 
-# CÁLCULOS PARA EL MÉTODO EXPLÍCITO
-num_puntos = len(z)
-num_anos = len(tiempos)
-U_explicito = np.full((num_puntos, num_anos), U0)
-U_explicito[0, :] = 0  # Condición de borde superior
-U_explicito[-1, :] = 0  # Condición de borde inferior
-
-# Cálculo de k
-k = Cv * 1 / (1**2)  # Se simplifica a dt=1 y dz=1 (Puede adaptarse mediante inputs)
-
-# Método explícito
-U_explicito = calcular_presion_poros_explicito(U_explicito, k, num_puntos, num_anos)
-
-df_resultados_explicitos = pd.DataFrame(U_explicito, columns=[f"{unidad_tiempo} {i+1}" for i in range(num_anos)],
-                                        index=[f"{i-1}" for i in range(1, num_puntos + 1)])
-df_resultados_explicitos = df_resultados_explicitos.round(2)
-# TABLAS DE RESULTADOS
-with col2:
-    st.header("Resultados - Teórico")
-    st.dataframe(df_resultados_teoricos)
-
-    st.header("Resultados - Método Explícito")
-    st.dataframe(df_resultados_explicitos)
-
-# ----- GRÁFICOS ----- 
-with col3:
-    st.header("GRÁFICOS")
-    fig, ax = plt.subplots(1, 1, figsize=(8, 6)) 
-    
-    # Colores
-    colores_teoricos = plt.cm.get_cmap('coolwarm', len(tiempos))
-    
-    # Gráfico método teórico
-    for idx, t in enumerate(tiempos):
+    for t in tiempos:
         Tv = calcular_Tv(Cv, t, Hdr)
         Uz = calcular_Uz(Tv, z_Hdr)
         Ue = calcular_Ue(Uz, U0)
-        ax.plot(Ue, z, label=f"Teórico {unidad_tiempo} {t}", color=colores_teoricos(idx))
-    
-    # Colores para el método explícito
-    for idx, j in enumerate(range(num_anos)):
-        ax.plot(df_resultados_explicitos.iloc[:, j], z, label=f"Explícito Año {j+1}", linestyle='--', color=colores_teoricos(idx))
-    
-    ax.axvline(x=U0, color='red', linestyle='--', label=f"Carga = {U0} kPa", lw=3) #Carga Inicial
+        
+        # Resultados de Ue para cada profundidad
+        for i, profundidad in enumerate(z):
+            resultados_teoricos[f"{unidad_tiempo} {t} "].append(Ue[i])
 
-    ax.set_xlim(left=0)
-    ax.set_ylim(0, H)
-    ax.set_xlabel("Presión de Poros (kPa)")
-    ax.set_ylabel("Profundidad (m)")
-    ax.invert_yaxis()
-    ax.grid()
-    ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
-    
-    st.pyplot(fig, use_container_width=True)
+    df_resultados_teoricos = pd.DataFrame(resultados_teoricos, index=z)
+    df_resultados_teoricos = df_resultados_teoricos.round(2)
+
+    # CÁLCULOS PARA EL MÉTODO EXPLÍCITO
+    num_puntos = len(z)
+    num_anos = len(tiempos)
+    U_explicito = np.full((num_puntos, num_anos), U0)
+    U_explicito[0, :] = 0  # Condición de borde superior
+    U_explicito[-1, :] = 0  # Condición de borde inferior
+
+    # Cálculo de k
+    k = Cv * 1 / (1**2)  # Se simplifica a dt=1 y dz=1 (Puede adaptarse mediante inputs)
+
+    # Método explícito
+    U_explicito = calcular_presion_poros_explicito(U_explicito, k, num_puntos, num_anos)
+
+    df_resultados_explicitos = pd.DataFrame(U_explicito, columns=[f"{unidad_tiempo} {i+1}" for i in range(num_anos)],
+                                            index=[f"{i-1}" for i in range(1, num_puntos + 1)])
+    df_resultados_explicitos = df_resultados_explicitos.round(2)
+
+    # TABLAS DE RESULTADOS
+    with col2:
+        st.header("Resultados - Teórico")
+        st.dataframe(df_resultados_teoricos)
+
+        st.header("Resultados - Método Explícito")
+        st.dataframe(df_resultados_explicitos)
+
+    # ----- GRÁFICOS ----- 
+    with col3:
+        st.header("GRÁFICOS")
+        fig, ax = plt.subplots(1, 1, figsize=(8, 6)) 
+        
+        # Colores
+        colores_teoricos = plt.cm.get_cmap('coolwarm', len(tiempos))
+        
+        # Gráfico método teórico
+        for idx, t in enumerate(tiempos):
+            Tv = calcular_Tv(Cv, t, Hdr)
+            Uz = calcular_Uz(Tv, z_Hdr)
+            Ue = calcular_Ue(Uz, U0)
+            ax.plot(Ue, z, label=f"Teórico {unidad_tiempo} {t}", color=colores_teoricos(idx))
+        
+        # Colores para el método explícito
+        for idx, j in enumerate(range(num_anos)):
+            ax.plot(df_resultados_explicitos.iloc[:, j], z, label=f"Explícito {unidad_tiempo} {j+1}", linestyle='--', color=colores_teoricos(idx))
+        
+        ax.axvline(x=U0, color='red', linestyle='--', label=f"Carga = {U0} kPa", lw=3) #Carga Inicial
+
+        ax.set_xlim(left=0)
+        ax.set_ylim(0, H)
+        ax.set_xlabel("Presión de Poros (kPa)")
+        ax.set_ylabel("Profundidad (m)")
+        ax.invert_yaxis()
+        ax.grid()
+        ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+        
+        st.pyplot(fig, use_container_width=True)
+
